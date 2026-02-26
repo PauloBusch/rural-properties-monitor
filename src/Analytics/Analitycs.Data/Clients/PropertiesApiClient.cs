b5cd11d43fb069e1b589dd3795a333c413d13961
M4theusVieir4 <vieiramatheus72@gmail.com>
2026-02-26 20:12:05 -0300

    Adicionando autenticação no endpoint que consome properties

--- src/Analytics/Analitycs.Data/Clients/PropertiesApiClient.cs
+++ src/Analytics/Analitycs.Data/Clients/PropertiesApiClient.cs
@@ -7,15 +7,22 @@ namespace Analitycs.Data.Clients;
 public class PropertiesApiClient : IPropertiesApiClient
 {
     private readonly HttpClient _httpClient;
-    public PropertiesApiClient(HttpClient httpClient)
+    private readonly IKeycloakTokenService _keycloakTokenService;
+    public PropertiesApiClient(HttpClient httpClient, IKeycloakTokenService keycloakTokenService)
     {
         _httpClient = httpClient;
+        _keycloakTokenService = keycloakTokenService;
     }
     public async Task<List<Property>> GetPropertiesByProducerAsync(string producerId, string token, CancellationToken cancellationToken)
     {
+        var accessToken = await _keycloakTokenService.GetAccessTokenAsync(cancellationToken);
+
         var url = $"https://localhost:44325/api/Properties/producer/{producerId}";
-        using var request = new HttpRequestMessage(HttpMethod.Get, url);
-        //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        using var request = new HttpRequestMessage(HttpMethod.Get, url);        
+
+        request.Headers.Authorization =
+            new AuthenticationHeaderValue("Bearer", accessToken);
+
         using var response = await _httpClient.SendAsync(request, cancellationToken);
 
         if (!response.IsSuccessStatusCode)
